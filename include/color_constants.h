17ab616ec4570cd2e39abb9ec7b16ed58c323632
Efimov Nikita <ne24kit@gmail.com>
2023-09-09 10:17:07 +0300

    Added printing of colored lines with define

--- include/color_constants.h
+++ include/color_constants.h
@@ -3,3 +3,5 @@
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define PRINT_COLORFUL_STRING(string, color)  printf(color string ANSI_COLOR_RESET)
